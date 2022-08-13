3f40227a3d2622c26338fe0b8db1566a63a8ed35
Anthony Merino <the.anthony.merino@gmail.com>
2022-08-13 14:24:43 -0600

    fixed process.env.PORT error

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = process.env.port || 8000;
+const port = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
   res.send('This is our express server for FEC');
