7b1b16e5c694eeebf265c322a450bb41742cca43
Javi <javiercamposcastello@gmail.com>
2022-08-13 14:01:13 -0500

    Nodemon implemented

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', () => {
+app.get('/', (req, res) => {
   res.send('This is our express server for FEC');
 })
 
