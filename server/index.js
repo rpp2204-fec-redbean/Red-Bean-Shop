336b3982a8ff9af87f10a562c1441623b52eb5d1
Anthony Merino <the.anthony.merino@gmail.com>
2022-08-14 17:42:03 -0600

    rendering_to_dom

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 require('dotenv').config();
+const path = require('path')
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8000;
+
+app.use(express.json())
+app.use(express.static(path.join(__dirname, '/../client/dist')));
 
 app.get('/', (req, res) => {
   res.send('This is our express server for FEC');
 })
+
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
