dd68252af57cf93b7c8ff544f161157bf5494df0
Javi <javiercamposcastello@gmail.com>
2022-08-16 19:49:47 -0500

    ProductInfo squeleton complete

--- client/src/components/Overview.jsx
+++ client/src/components/Overview.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { list_product, product_information, product_styles, related_products } from '../../example_data/example.js';
+import { list_product, product_information, product_styles, related_products, product_reviews } from '../../example_data/example.js';
 import ProductInfo from './ProductInfo.jsx';
 
 
@@ -7,9 +7,7 @@ function Overview () {
 
   return (
     <div>
-      <div>This is the Overview component</div>
       <ProductInfo />
-      <div>{list_product[0].id}</div>
     </div>
   )
 }
--- client/src/components/ProductInfo.jsx
+++ client/src/components/ProductInfo.jsx
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { list_product, product_information, product_styles, related_products } from '../../example_data/example.js';
+import { list_product, product_information, product_styles, related_products, product_reviews } from '../../example_data/example.js';
 
 
 function ProductInfo () {
 
   return (
     <div>
+      <div className='product_info_reviews'>
+        <div>This product has 4 stars reviews</div>
+        <button>Read all reviews</button>
+      </div>
+      <div>{list_product[0].category}</div>
+      <div>{list_product[0].name}</div>
+      <div>{list_product[0].default_price}</div>
       <h4>{list_product[0].slogan}</h4>
       <div>{list_product[0].description}</div>
       <ul>
@@ -17,6 +24,7 @@ function ProductInfo () {
           })
         }
       </ul>
+      <button>Add to My Outfit</button>
 
     </div>
   )
