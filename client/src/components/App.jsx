7c576a784189c4310ea34f32d922adbb3beebe94
Riccardo Pirruccio <rickp1795@gmail.com>
2022-08-17 22:50:39 -0500

    set up eslint and configed with prettier and air-bnb style guide

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import ReviewsModule from './reviews_module/ReviewsModule.jsx'
 
-function App (props) {
+function App(props) {
   return (
     <div>
       <h1> The RedBean Atelier App </h1>
       <ReviewsModule />
     </div>
-  )
+  );
 }
 
 export default App;
