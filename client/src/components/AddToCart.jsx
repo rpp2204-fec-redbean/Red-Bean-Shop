2dd0fbab09fde4afc6c49fee628e84f236eca379
Javi <javiercamposcastello@gmail.com>
2022-08-18 22:17:23 -0500

    Size dropdown complete

--- client/src/components/AddToCart.jsx
+++ client/src/components/AddToCart.jsx
@@ -3,15 +3,29 @@ import React, { useState, useEffect } from 'react';
 function AddToCart (props) {
 
   const [styleAvail, setStyleAvail] = useState({});
+  const [outOfStock, setOutOfStock] = useState(true);
+
   var availability = async (skus) => {
     var resultObj = {};
     const skusArray = await Object.entries(skus);
-    skusArray.forEach((size) => {
+      skusArray.forEach((size) => {
       var sizesArray = Object.entries(size[1]);
       resultObj[sizesArray[1][1]] = sizesArray[0][1];
     });
-    console.log(resultObj);
     setStyleAvail(resultObj);
+
+    for (const size in resultObj) {
+      if (resultObj[size] > 0) {
+        setOutOfStock(false);
+      }
+    }
+    // resultObj.forEach((qty) => {
+    //   if (qty > 0) {
+    //     outOfStock = false;
+    //   }
+    // });
+    console.log(outOfStock);
+
   };
 
   useEffect(() => {
@@ -19,14 +33,26 @@ function AddToCart (props) {
     availability(props.style.skus);
   }, [props.style]);
 
-  return (
-    <div className='add-to-cart'>
-      <div>The selected style id is</div>
-      <select>
+  if (outOfStock === false) {
+    return (
+      <div className='add-to-cart'>
+        <div>The selected style id is</div>
+        <select selected='Select Size'>
+          <option value="" selected disabled hidden>Select Size</option>
+          {
+            Object.keys(styleAvail).map((size) => {
+              if (styleAvail[size] !== 0) {
+                return (
+                  <option value={size}>{size}</option>
+                )
+              }
+            })
+          }
+        </select>
+      </div>
+    )
 
-      </select>
-    </div>
-  )
+  }
 }
 
 export default AddToCart;
