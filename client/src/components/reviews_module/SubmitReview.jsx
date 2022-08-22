992ee156ca2315ea1ce74f628ad3afc3de601012
Anthony Merino <the.anthony.merino@gmail.com>
2022-08-21 18:16:55 -0600

    add fontawesome functionality for react, stars to the overall rating in review modal

--- client/src/components/reviews_module/SubmitReview.jsx
+++ client/src/components/reviews_module/SubmitReview.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid, regular } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
 
 function SubmitReview ( {showReviewModal, setShowReviewModal, submitReview, productName} ) {
 
@@ -24,6 +26,18 @@ function SubmitReview ( {showReviewModal, setShowReviewModal, submitReview, prod
         <h1>Write Your Review</h1>
         <h3>About the {productName}</h3>
 
+        {/* This div will ask the customer to fill in a star rating */}
+        <div id="rate-by-star">
+          <fieldset>
+            <legend>Overall Rating</legend>
+              <FontAwesomeIcon icon={solid('star')}  id="star-1" />
+              <FontAwesomeIcon icon={solid('star')}  id="star-2" />
+              <FontAwesomeIcon icon={solid('star')}  id="star-3" />
+              <FontAwesomeIcon icon={solid('star')}  id="star-4" />
+              <FontAwesomeIcon icon={regular('star')} id="star-5" />
+          </fieldset>
+        </div>
+
         {/* This div will ask the customer if they recommend the product*/}
         <div id='recommend'>
           <fieldset>
@@ -33,17 +47,34 @@ function SubmitReview ( {showReviewModal, setShowReviewModal, submitReview, prod
                 <input type='radio' name='rec' value='yes' onClick={() => handleRecommend('true')}
                 /> Yes
               </label>
-            </div>
-            <div>
               <label>
                 <input type='radio' name='rec' value='no' onClick={() => handleRecommend('false')}
                 /> No
               </label>
             </div>
           </fieldset>
+        </div>
+      {/* // This div asks the customer about different product characteristics */}
+      <div id="characteristics-radios">
+        <fieldset>
+          <legend>Characteristics</legend>
+            <div>
+              <label> Quality
+                <input type='radio' name='quality' value='1' onClick={() => console.log('quality 1')}/>
+                <input type='radio' name='quality' value='2' onClick={() => console.log('quality 2')}/>
+                <input type='radio' name='quality' value='3' onClick={() => console.log('quality 3')}/>
+                <input type='radio' name='quality' value='4' onClick={() => console.log('quality 4')}/>
+                <input type='radio' name='quality' value='5' onClick={() => console.log('quality 5')}/>
+              </label>
+            </div>
+        </fieldset>
           <br />
           <input type='submit' onClick={() => handleSubmit()}></input>
-        </div>
+      </div>
+
+
+
+
 
       </div>
     </div>
