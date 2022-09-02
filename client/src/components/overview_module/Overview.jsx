95a269479bad9b1e34cca698b1c92e127f9bb59e
Javi <javiercamposcastello@gmail.com>
2022-09-01 20:57:27 -0500

    Working on star reviews

--- client/src/components/overview_module/Overview.jsx
+++ client/src/components/overview_module/Overview.jsx
@@ -11,6 +11,7 @@ import Styles from './Styles.jsx';
 import AddToCart from './AddToCart.jsx';
 import Gallery from './Gallery.jsx';
 import axios from 'axios';
+import
 
 function Overview(props) {
   const [product, setProduct] = useState({});
@@ -62,6 +63,10 @@ function Overview(props) {
       setStyles(data.data);
     });
   }, []);
+
+  useEffect(() = > {
+    axios.get(`/reviews`)
+  }, [])
 
   if (Object.keys(product).length) {
     return (
