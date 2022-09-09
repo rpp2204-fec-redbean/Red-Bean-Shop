907c9fffa1bb8a38f87d4e2109d08a7651555f22
chiElephant <anthony@Anthonys-MacBook-Pro.local>
2022-09-09 01:47:23 -0600

    added description under product bd graph

--- client/src/components/reviews_module/helper_functions/product_bd.js
+++ client/src/components/reviews_module/helper_functions/product_bd.js
@@ -4,26 +4,56 @@ import { solid, regular, light, thin } from '@fortawesome/fontawesome-svg-core/i
 function createCharsDiv(metadata) {
   const charDiv = [];
 
+
   for (let char in metadata) {
+
     if (metadata[char] !== null) {
+
       const key = metadata[char].id;
       const value = Math.floor(metadata[char].value);
+      const element = document.getElementById("breakdown-descrip");
 
-      charDiv.push(
-        <div className="characteristic" key={key}>
-          <div className="char-name">
-            {`${char}`}
+      if (char === 'Comfort' || char === 'Quality') {
+        charDiv.push(
+          <div className="characteristic" key={key}>
+            <div className="char-name">
+              {`${char}`}
+            </div>
+            <div className="char-meter">
+              <FontAwesomeIcon
+                id="char-icon"
+                icon={(solid('triangle'))}
+                style={{width: (value * 40) + '%'}}
+                flip='vertical'
+                />
+            </div>
+            <div id="breakdown-descrip">
+              <div id="poor">Poor</div>
+              <div id="great">Great</div>
+            </div>
+          </div>)
+      } else {
+        charDiv.push(
+          <div className="characteristic" key={key}>
+            <div className="char-name">
+              {`${char}`}
+            </div>
+            <div className="char-meter">
+              <FontAwesomeIcon
+                id="char-icon"
+                icon={(solid('triangle'))}
+                style={{width: (value * 40) + '%'}}
+                flip='vertical'
+                />
+            </div>
+            <div id="breakdown-descrip">
+              <div id="too-small">Too Small</div>
+              <div id="perfect">Perfect</div>
+              <div id="too-big">Too Big</div>
+            </div>
           </div>
-          <div className="char-meter">
-            <FontAwesomeIcon
-              id="char-icon"
-              icon={(solid('triangle'))}
-              style={{width: (value * 40) + '%'}}
-              flip='vertical'
-            />
-          </div>
-        </div>
-      );
+        );
+      }
     }
   }
 
