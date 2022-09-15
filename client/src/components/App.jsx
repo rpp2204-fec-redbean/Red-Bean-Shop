74553885c6d848fa16ff3629445ea0007cfd8695
Riccardo Pirruccio <rickp1795@gmail.com>
2022-09-15 18:01:38 -0500

    uncommented reviews module to run google pages test

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -41,7 +41,7 @@ function App() {
     productId !== 0 ? (
       <>
         <Overview product_id={productId} />
-        {/* <ReviewsModule product_id={productId} product_name={productName} /> */}
+        <ReviewsModule product_id={productId} product_name={productName} />
         <QandAModule product_id={productId} product_name={productName} />
       </>
     ) : null;
