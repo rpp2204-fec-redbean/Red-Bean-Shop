032f26785c144d591b26cc54014ee0c425e307b0
Riccardo Pirruccio <rickp1795@gmail.com>
2022-09-17 10:38:05 -0500

    fixed error of accidental commit on ratingsBreakdown and ratings_bd

--- client/src/components/reviews_module/RatingsBreakdown.jsx
+++ client/src/components/reviews_module/RatingsBreakdown.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useMemo, useRef } from 'react';
 import ProductBreakdown from './ProductBreakdown.jsx';
-
 import helpers from './helper_functions/ratings_bd.js';
-
 function RatingsBreakdown({
   product_id,
   setCharacteristics,
@@ -28,20 +26,16 @@ function RatingsBreakdown({
   useEffect(() => {
     helpers.getMetadata(product_id, (meta) => {
       metadata.current = meta;
-
       helpers.handleRatings(meta.ratings, (avg, reviewsCount) => {
         totalReviews.current = reviewsCount;
         avgRating.current = avg;
       });
-
       helpers.handleRecommended(meta.recommended, (avg) => {
         percentRecommended.current = avg;
       });
-
       setCharacteristics(() => meta.characteristics);
     });
   }, [product_id]);
-
   return (
     <div id="ratings-breakdown" fromelement="Ratings/Reviews">
       <div id="average-rating">
@@ -67,5 +61,4 @@ function RatingsBreakdown({
     </div>
   );
 }
-
 export default RatingsBreakdown;
