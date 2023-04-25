5db8264ad3324d516ccad6408b2c57b438dfc3c0
Riccardo Pirruccio <rickp1795@gmail.com>
2023-04-25 18:23:41 -0500

    refactored topbar component to render on page load before selecting a product

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Topbar from './components/Topbar.jsx';
 import App from './components/App.jsx';
 
 const container = document.getElementById('root');
@@ -12,6 +13,7 @@ const root = ReactDOM.createRoot(container);
 root.render(
   <BrowserRouter>
     <Routes>
+      <Route path="/" element={<Topbar />} />
       <Route path="/:id" element={<App />} />
     </Routes>
   </BrowserRouter>
